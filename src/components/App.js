18eae7bb2bbbc099b07499201993d5454c299388
Ingrid Amaral <guid_95@hotmail.com>
2020-03-09 19:03:31 +0100

    replacing redux with context

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LaguageSelector';
 
 class App extends React.Component {
     state = { language: 'english'}
@@ -12,21 +13,13 @@ class App extends React.Component {
     render() {
         return (
             <div className='ui container'>
-                <div>
-                    Select a language:
-                    <i 
-                        onClick={() => this.onLanguageChange('english')} 
-                        className='flag us' />
-                    <i 
-                        onClick={() => this.onLanguageChange('dutch')} 
-                        className='flag nl' />
-                </div>
+                <LanguageSelector onLanguageChange={this.onLanguageChange} />
+                
                 <ColorContext.Provider value='red'>
                     <LanguageContext.Provider value={this.state.language}>
                         <UserCreate />
                     </LanguageContext.Provider>
                 </ColorContext.Provider>
-                
             </div>
         )
     }
