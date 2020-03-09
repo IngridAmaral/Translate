ee91f91d8c82653f3ca4682f5f2d86f536f6f31b
Ingrid Amaral <guid_95@hotmail.com>
2020-03-09 18:41:23 +0100

    pulling from multiple contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate'
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
     state = { language: 'english'}
@@ -13,12 +14,19 @@ class App extends React.Component {
             <div className='ui container'>
                 <div>
                     Select a language:
-                    <i onClick={() => this.onLanguageChange('english')} className='flag us' />
-                    <i onClick={() => this.onLanguageChange('dutch')} className='flag nl' />
+                    <i 
+                        onClick={() => this.onLanguageChange('english')} 
+                        className='flag us' />
+                    <i 
+                        onClick={() => this.onLanguageChange('dutch')} 
+                        className='flag nl' />
                 </div>
-                <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate />
-                </LanguageContext.Provider>
+                <ColorContext.Provider value='red'>
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
+                
             </div>
         )
     }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export default React.createContext('english')
+export default React.createContext('english');
+
+// const context = React.createContext('english')
+
+// export default context;
